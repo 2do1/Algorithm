a5f44632500a8d65ca11340f320e0056a70817f8
2do1 <dowon3036@naver.com>
2022-04-19 12:07:11 +0900

    추가: 하샤드 수 solution.py README.md

---
+++ Programmers/Level1/하샤드 수/solution.py
@@ -0,0 +1,14 @@
+def solution(x):
+    answer = True
+    
+    digits_sum = 0 # 모든 자릿수의 합
+    
+    for num in str(x): # string 으로 변환 후 접근
+        digits_sum += int(num) # string 형이기 때문에 int 형으로 변환
+                   
+    if x % digits_sum == 0: # 나누어 떨어지면 하샤드 수
+        answer = True
+    else:
+        answer = False
+         
+    return answer
