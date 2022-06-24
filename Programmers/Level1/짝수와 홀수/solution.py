09b60f0e4da845f7430fd11431e833d43ea598ec
2do1 <dowon3036@naver.com>
2022-06-24 21:07:26 +0900

    추가: 짝수와 홀수 solution.py README.md

---
+++ Programmers/Level1/짝수와 홀수/solution.py
@@ -0,0 +1,9 @@
+def solution(num):
+    return "Even" if num % 2 == 0 else "Odd"
+
+    # answer = ''
+    # if num % 2 == 0: 
+    #     answer = "Even" 
+    # else: 
+    #     answer = "Odd"
+    # return answer
