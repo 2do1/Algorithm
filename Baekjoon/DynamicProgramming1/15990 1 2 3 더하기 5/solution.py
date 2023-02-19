99ab5e8703b4846f1a75720c0642035a22f230a8
2do1 <dowon3036@naver.com>
2023-02-19 23:57:08 +0900

    수정: 15990 1 2 3 더하기 5 solution.py

--- Baekjoon/DynamicProgramming1/15990 1 2 3 더하기 5/solution.py
+++ Baekjoon/DynamicProgramming1/15990 1 2 3 더하기 5/solution.py
@@ -8,13 +8,11 @@ dp[1] = [1, 0, 0]
 dp[2] = [0, 1, 0]
 dp[3] = [1, 1, 1]
 
-
 for index in range(4, 100001):
     dp[index][0] = (dp[index - 1][1] + dp[index - 1][2]) % 1000000009
     dp[index][1] = (dp[index - 2][0] + dp[index - 2][2]) % 1000000009
     dp[index][2] = (dp[index - 3][0] + dp[index - 3][1]) % 1000000009
 
-print(dp[:7])
 for _ in range(t):
     n = int(sys.stdin.readline())
 
